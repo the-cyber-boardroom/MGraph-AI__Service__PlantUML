a1db24285ce76784180d3f4564ebbc800db0db3e
Dinis Cruz <dinis.cruz@owasp.org>
2025-12-17 18:08:43 +0000

    fixed lambda config

--- mgraph_ai_service_plantuml/config.py
+++ mgraph_ai_service_plantuml/config.py
@@ -1,6 +1,6 @@
 from mgraph_ai_service_plantuml import package_name
 
 SERVICE_NAME                       = package_name
-FAST_API__TITLE                          = "MGraph AI Service PlantUML"
-FAST_API__DESCRIPTION              = "Base template for MGraph-AI microservices"
-LAMBDA_DEPENDENCIES__BASE__SERVICE = ['osbot-fast-api-serverless==v1.2.0']
+FAST_API__TITLE                    = "MGraph AI Service PlantUML"
+FAST_API__DESCRIPTION              = "PlantUML Service"
+LAMBDA_DEPENDENCIES__BASE__SERVICE = ['osbot-fast-api-serverless==v1.32.0']
